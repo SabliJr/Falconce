4f17618845ae7d1646f01e1cbe1288fecb14b181
Sabli Jr <sablijr@yahoo.com>
2023-02-19 20:01:18 +0100

    Styling the status

--- FrontEnd/src/Components/Status/MarketStatus.jsx
+++ FrontEnd/src/Components/Status/MarketStatus.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./CryptoStatus.css";
 import millify from "millify";
 
+import { RiArrowDropDownFill, RiArrowDropUpFill } from "react-icons/ri";
+
 const MarketStatus = ({ gs }) => {
   return (
     <>
@@ -11,16 +13,39 @@ const MarketStatus = ({ gs }) => {
         <span>{gs.active_cryptocurrencies.toLocaleString()}</span>&nbsp;
         across&nbsp;
         <span>{gs.active_exchanges}</span>&nbsp;exchanges globally, with a total
-        market cap of <span>${millify(gs.quote.USD.total_market_cap)}</span>, an
-        increase of&nbsp;
-        <span>
+        market cap of <span>${millify(gs.quote.USD.total_market_cap)}</span>,
+        a&nbsp;
+        <div
+          className={
+            gs.quote.USD.total_market_cap_yesterday_percentage_change < 0
+              ? "statusDown"
+              : "statusUp"
+          }>
+          {gs.quote.USD.total_market_cap_yesterday_percentage_change < 0 ? (
+            <RiArrowDropDownFill className='statusIcon' />
+          ) : (
+            <RiArrowDropUpFill className='statusIcon' />
+          )}
           {gs.quote.USD.total_market_cap_yesterday_percentage_change.toLocaleString()}
           %
-        </span>{" "}
-        in the last 24 hours. Bitcoin’s dominance is currently&nbsp;
-        <span>{gs.btc_dominance.toLocaleString()}%</span>, an increase of&nbsp;
-        <span>{gs.btc_dominance_24h_percentage_change.toLocaleString()}%</span>
-        &nbsp; over the day.
+        </div>
+        &nbsp; change in the last 24 hours. Bitcoin’s dominance is
+        currently&nbsp;
+        <span>{gs.btc_dominance.toLocaleString()}%</span>, a change of&nbsp;
+        <div
+          className={
+            gs.btc_dominance_24h_percentage_change < 0
+              ? "statusDown"
+              : "statusUp"
+          }>
+          {gs.btc_dominance_24h_percentage_change < 0 ? (
+            <RiArrowDropDownFill className='statusIcon' />
+          ) : (
+            <RiArrowDropUpFill className='statusIcon' />
+          )}
+          {gs.btc_dominance_24h_percentage_change.toLocaleString()}%
+        </div>
+        &nbsp; as well over the day.
       </p>
     </>
   );
