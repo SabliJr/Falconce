ca031e8dc8700b1888ccd93fe93cff69b85ce102
Sabli Jr <sablijr@yahoo.com>
2023-03-02 09:44:48 +0100

    Adding the back drop

--- FrontEnd/src/Components/Navbar/SearchData.jsx
+++ FrontEnd/src/Components/Navbar/SearchData.jsx
@@ -12,30 +12,36 @@ const SearchData = () => {
     navigate(`coin-page/${id}`);
   };
 
+  window.addEventListener("click", () => {
+    setSearchCoin("");
+  });
+
   return (
-    <ul className='PList'>
-      {searchResult?.map((coin) => {
-        if (searchCoin.length < 3) {
-          return null;
-        } else {
-          return (
-            <li
-              key={coin.id}
-              className='searchList'
-              onClick={() => {
-                searchedCoin(coin.id);
-                setSearchCoin("");
-              }}>
-              <img src={coin.large} alt='searchImg' className='searchImg' />
-              <div className='searchNameDiv'>
-                <p className='searchSymbol'>{coin.symbol.toUpperCase()}</p>
-                <p className='searchName'>{coin.name}</p>
-              </div>
-            </li>
-          );
-        }
-      })}
-    </ul>
+    <div className={searchCoin.length < 3 ? "panel removePanel" : "panel"}>
+      <ul className='PList'>
+        {searchResult?.map((coin) => {
+          if (searchCoin.length < 3) {
+            return null;
+          } else {
+            return (
+              <li
+                key={coin.id}
+                className='searchList'
+                onClick={() => {
+                  searchedCoin(coin.id);
+                  setSearchCoin("");
+                }}>
+                <img src={coin.large} alt='searchImg' className='searchImg' />
+                <div className='searchNameDiv'>
+                  <p className='searchSymbol'>{coin.symbol.toUpperCase()}</p>
+                  <p className='searchName'>{coin.name}</p>
+                </div>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
   );
 };
 
